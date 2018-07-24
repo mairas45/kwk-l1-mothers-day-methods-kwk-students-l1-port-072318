c16fee08ae92eed735f02bd64bfcf8636f75d94a
mairas45 <mairasackitey45@icloud.com>
2018-07-24 20:05:16 +0000

    automatically backed up by learn

--- conditionals.rb
+++ conditionals.rb
@@ -1,13 +1,13 @@
+grade = gets.strip.upcase
 
-
-# if grade == "A" || grade > 90
-#   puts "awesome job! ;)"
-# elsif grade =="B"
-#   puts "good job"
-# elsif grade == "C"
-#   puts "you are passing!"
-# elsif grade == "F" || grade < 60
-# puts "try harder"
-# else
-#   puts "please re-enter your grade as a letter"
-# end
+if grade.to_s == "A" || grade.to_i > 90
+  puts "awesome job! ;)"
+elsif grade.to_s =="B"
+  puts "good job"
+elsif grade.to_s == "C"
+  puts "you are passing!"
+elsif grade.to_s == "F" || grade.to_i < 60
+puts "try harder"
+else
+  puts "please re-enter your grade as a letter"
+end
