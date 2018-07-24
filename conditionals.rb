9e5f022d77b9f6068c3ffbbfab3160045caea024
mairas45 <mairasackitey45@icloud.com>
2018-07-24 20:23:24 +0000

    automatically backed up by learn

--- conditionals.rb
+++ conditionals.rb
@@ -4,7 +4,6 @@ grade = gets.strip.upcase
 if grade.to_s == "A" || grade.to_i > 90
   puts "awesome job! ;)"
 elsif grade.to_s =="B" || (grade.to_i >= 80 && grade.to_i <=89)
-
 elsif grade.to_s == "C"|| (grade.to_i >= 70 && grade.to_i <=79)
 elsif grade.to_s == "D"|| (grade.to_i >= 60 && grade.to_i <=69)
 elsif grade.to_s == "F" || grade.to_i < 60
