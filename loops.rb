ce651069ec7ce9934b459f5af9392b3883dce892
mairas45 <mairasackitey45@icloud.com>
2018-07-24 21:54:22 +0000

    automatically backed up by learn

--- loops.rb
+++ loops.rb
@@ -1,12 +1,11 @@
 
-pizza_slices = 8
+# pizza_slices = 8
 
-while pizza_slices > 0 
-  puts "you currently have #{pizza_slices} !" "now take one" 
-  sleep(2)
-  puts "now you have #{pizza_slices - 1} slices left!"
-  pizza_slices = pizza_slices - 1 
-end 
-
+# while pizza_slices > 0 
+#   puts "you currently have #{pizza_slices} !" "now take one" 
+#   sleep(2)
+#   puts "now you have #{pizza_slices - 1} slices left!"
+#   pizza_slices -= 1                       #this is the counter
+# end 
 
 
