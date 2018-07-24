6b70b4cc464db62442a5748735b288648a42e2d1
mairas45 <mairasackitey45@icloud.com>
2018-07-24 20:19:52 +0000

    automatically backed up by learn

--- conditionals.rb
+++ conditionals.rb
@@ -7,7 +7,7 @@ elsif grade.to_s =="B" || (grade.to_i >= 80 && grade.to_i <=89)
   puts "good job"
 elsif grade.to_s == "C"|| (grade.to_i >= 70 && grade.to_i <=79)
   puts "its a passing grade, but i know you can do better!"
-elsif
+elsif grad4
 elsif grade.to_s == "F" || grade.to_i < 60
 puts "try harder"
 else
