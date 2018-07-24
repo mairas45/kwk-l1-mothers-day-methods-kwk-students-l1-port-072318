f9796f12807e7ba334cbd8331c486607e6a75282
mairas45 <mairasackitey45@icloud.com>
2018-07-24 21:10:45 +0000

    automatically backed up by learn

---
+++ fizz_buzz.rb
@@ -0,0 +1,7 @@
+# goals: create a program that recieves any number and prints
+# prints "fizz" if the number is divisible by 3
+# prints "buzz" if the number is divisible by 5
+# prints "fizzbuzz" if the number is divisible by 3 & 5
+
+#use modulus (%) to determine the remainder of any division expression
+puts 11 % 5
