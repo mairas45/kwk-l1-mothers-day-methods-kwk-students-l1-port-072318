2d9f99f83112e53519a54941ade20ecde02e3d0f
mairas45 <mairasackitey45@icloud.com>
2018-07-24 22:04:24 +0000

    automatically backed up by learn

--- loops.rb
+++ loops.rb
@@ -9,3 +9,13 @@
 # end 
 
 
+andrews_texts_to_his_friend = 0 
+
+until andrews_texts_to_his_friend == 10
+puts "do you really want to text this person?"
+puts "why dont you bake some bread instead"
+puts "you have #{10 - andrews_texts_to_his_friend} texts left"
+  
+  andrews_texts_to_his_friend +=1 
+
+end 
