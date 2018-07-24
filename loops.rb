a92283c6421962ff178bdd90e0bbe63e3e774cf1
mairas45 <mairasackitey45@icloud.com>
2018-07-24 21:23:10 +0000

    automatically backed up by learn

--- loops.rb
+++ loops.rb
@@ -5,6 +5,7 @@ while pizza_slices > 0
   puts "you currently have #{pizza_slices} !" "now take one" 
   sleep(2)
   puts "now you have #{pizza_slices - 1} slices left!"
+  pizza_slices = pizza_slices - 1 
 end 
 
 
